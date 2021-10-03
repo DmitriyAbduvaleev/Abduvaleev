52bb8575d69a7eb6688711b132dce8b01bc74780
DmitriyAbduvaleev <90891322+DmitriyAbduvaleev@users.noreply.github.com>
2021-10-03 18:45:51 +0300

    Add files via upload

---
+++ 05_name/05_name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	char name[45];
+	cout << "Please, enter you name: ";
+	cin >> name;
+	cout << "Hello,  " << name << "! \n";
+	system ("pause");
+    return 0;
+}
